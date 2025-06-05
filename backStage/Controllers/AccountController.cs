a64edbf8f98a8d9ffaf10bc8d5bbc051ebfe0856
user <user@DESKTOP-NU0STEI>
2025-06-05 21:16:57 +0800

    加入專案檔案。

---
+++ backStage/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace backStage.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            // todo-->去資料庫檢查帳密
+            bool isValid = (username == "admin" && password == "1234");
+
+            if (isValid)
+            {
+                // 登入成功，進後台主頁
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                // 登入失敗，顯示錯誤訊息
+                ViewBag.ErrorMessage = "帳號或密碼錯誤";
+                return View();
+            }
+        }
+    }
+}
---
+++ backStage/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace backStage.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
