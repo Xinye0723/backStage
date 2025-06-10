0d88d99ccf31ea3bf0f9c5bb6fba992040b30350
xinye <54725472a76@gmail.com>
2025-06-10 09:23:33 +0800

    debug

--- backStage/Models/MovieContext.cs
+++ backStage/Models/MovieContext.cs
@@ -10,10 +10,7 @@ public partial class MovieContext : DbContext
     {
     }
 
-    public MovieContext(DbContextOptions<MovieContext> options)
-        : base(options)
-    {
-    }
+
 
     public virtual DbSet<FavoriteMovie> FavoriteMovies { get; set; }
 
@@ -48,7 +45,6 @@ public partial class MovieContext : DbContext
     public virtual DbSet<Tag> Tags { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=movie;Integrated Security=true;Encrypt=true;TrustServerCertificate=true;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
