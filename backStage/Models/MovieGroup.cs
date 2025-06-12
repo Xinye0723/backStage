579a2175c66c313718d2d872ad047cf364c30110
Wang <cvbza6425@yahoo.com.tw>
2025-06-12 16:17:40 +0800

    te

--- backStage/Models/MovieGroup.cs
+++ backStage/Models/MovieGroup.cs
@@ -1,26 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace backStage.Models;
 
 public partial class MovieGroup
 {
+    [Display(Name = "群組id")]
     public int GroupId { get; set; }
 
+    [Display(Name = "電影id")]
     public int MovieId { get; set; }
 
+    [Display(Name = "放映時間id")]
     public int ShowTimeId { get; set; }
 
+    [Display(Name = "活動名稱")]
     public string? GroupName { get; set; }
 
+    [Display(Name = "主辦人id")]
     public int? LeaderMemberId { get; set; }
 
+    [Display(Name = "最多人數")]
     public int? MaxMembers { get; set; }
 
+    [Display(Name = "活動照片")]
     public string? GroupNote { get; set; }
 
+    [Display(Name = "創建時間")]
     public DateTime? CreateTime { get; set; }
 
+    [Display(Name = "活動介紹")]
     public string? Status { get; set; }
 
     public virtual ICollection<Registration> Registrations { get; set; } = new List<Registration>();
